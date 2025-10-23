244760ea6a9bad24abb75559f6860d67d6711673
shivansh <53109024+Decodx09@users.noreply.github.com>
2025-10-23 13:54:01 +0530

    just takei t

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 set -e
 
-mkdir -p /output
 truncate -s 8G /output/pi-image.img
 
-sfdisk /output/pi-image.img << SFDISK_EOF
-label: dos
-,256M,c,*
-,,L,
-SFDISK_EOF
+# This setting helps guestfish run reliably in containers
+export LIBGUESTFS_BACKEND=direct
 
-LOOP_DEV=$(losetup -fP --show /output/pi-image.img)
-BOOT_PART="${LOOP_DEV}p1"
-ROOT_PART="${LOOP_DEV}p2"
+# Use guestfish to partition, format, and copy files into the image
+guestfish --add /output/pi-image.img <<_EOF_
+run
+part-init /dev/sda mbr
+part-add /dev/sda primary fat32 2048 526335
+part-add /dev/sda primary ext4 526336 -1
+part-set-bootable /dev/sda 1
+mkfs vfat /dev/sda1 label:boot
+mkfs ext4 /dev/sda2 label:root
+mount /dev/sda2 /
+tar-in /rootfs.tar / compress:gzip
+mount /dev/sda1 /boot
+tar-in /bootfs.tar /boot compress:gzip
+_EOF_
 
-mkfs.vfat -F32 "$BOOT_PART"
-mkfs.ext4 "$ROOT_PART"
-
-mkdir -p /mnt/root
-mount "$ROOT_PART" /mnt/root
-cp -a /rootfs/. /mnt/root/
-
-mkdir -p /mnt/root/boot/firmware
-mount "$BOOT_PART" /mnt/root/boot/firmware
-cp -a /bootfs/. /mnt/root/boot/firmware/
-
-umount /mnt/root/boot/firmware
-umount /mnt/root
-losetup -d "$LOOP_DEV"
-
-echo "Image creation complete!"
+echo "Image creation complete with guestfish!"
